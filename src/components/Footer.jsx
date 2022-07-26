3dcfcdb8d7c7f8b85677beca89445c72626bdc2b
woojoung <ywj981217@naver.com>
2022-07-26 20:41:22 +0900

    react shopping toyproject

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2022 WooJoung YOON. All rights reserved.</p>
+    </footer>
+  );
+}
---
+++ src/components/Prototypes.jsx
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+import AppStateContext from "../contexts/AppStateContext";
+
+export default function Prototypes() {
+  const { prototypes } = useContext(AppStateContext);
+  return (
+    <main>
+      <div className="prototypes">
+        {" "}
+        {prototypes.map((prototype) => {
+          const { id, thumbnail, title, price, desc, pieUrl } = prototype;
+          return (
+            <div className="prototype" key={id}>
+              <a href={pieUrl} target="_BLANK" rel="noreferrer">
+                <div
+                  style={{
+                    padding: "25px 0 33px 0 ",
+                  }}
+                >
+                  <video
+                    autoPlay
+                    loop
+                    playsInline
+                    className="prototype__artwork prototype__edit"
+                    style={{
+                      objectFit: "contain",
+                    }}
+                    src={thumbnail}
+                  />
+                </div>
+              </a>
+
+              <div className="prototype__body">
+                <div className="prototype__title">
+                  <div className="btn btn--primary float--right">
+                    <i className="icon icon--plus"></i>
+                  </div>
+                  {title}
+                </div>
+                <p className="prototype__price">$ {price}</p>
+                <p className="prototype__desc">{desc}</p>
+              </div>
+            </div>
+          );
+        })}{" "}
+      </div>
+    </main>
+  );
+}
